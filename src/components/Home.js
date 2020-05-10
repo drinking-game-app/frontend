986474dc8fceced674fb2bf1dfbe9b550b1f3e63
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-05-10 16:03:22 +0100

    Added swipe capability

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Text, View, SafeAreaView, FlatList } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { Component } from "react";
+import { Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, TouchableOpacity, FlatList, RectButton } from "react-native-gesture-handler";
+
+
 
 /**
  * Importing styles
@@ -9,47 +11,126 @@ import { TouchableOpacity } from "react-native-gesture-handler";
  */
 const styles = require("../themes")("App");
 
-const reasonsArr = [
-  { id: 0, text: "he smells" },
-  { id: 1, text: "he is bad at C++" },
-  { id: 2, text: "nobody really likes him" },
-  { id: 3, text: "because me eat burger and angry" },
-];
-
 /**
  * Rendering the view
  */
-export default () => (
-  <SafeAreaView style={styles.container}>
-    <Text style={styles.title}>Drinking Game App</Text>
-    <View>
-      <TouchableOpacity>
-        <Text style={styles.title}>
-          Reasons why people don't like <Text style={styles.accent}>Ryan</Text>:
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.itemContainer}>
-        <FlatList
-          data={reasonsArr}
-          renderItem={({ item }) => <Item text={item.text} />}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-    </View>
-  </SafeAreaView>
-);
+export default class Home extends Component {
+  state = {
+    text: 'Drinking Game App',
+    reasonsArr: [
+      { id: 0, text: "he smells" },
+      { id: 1, text: "he is bad at C++" },
+      { id: 2, text: "nobody really likes him" },
+      { id: 3, text: "because me eat burger and angry" },
+    ]
+  }
+
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>{this.state.text}</Text>
+        <View>
+          <TouchableOpacity>
+            <Text style={styles.title}>
+              Reasons why people don't like <Text style={styles.accent}>Ryan</Text>:
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.itemContainer}>
+            <FlatList
+              data={this.state.reasonsArr}
+              renderItem={({ item, index }) => <Row item={item.text} index={index} />}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    )
+  }
+}
 
 /**
  * Renders a single item in the flatlist
  * 
  * @param {String} text
  */
-function Item({ text }) {
-  return (
-    <TouchableOpacity>
-      <View style={styles.item}>
-        <Text>{text}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+const Item = ({ text }) => (
+    <RectButton style={styles.rectButton}>
+      <Text style={styles.fromText}>{text}</Text>
+      <Text numberOfLines={2} style={styles.messageText}>
+        Swipe me left or right
+      </Text>
+    </RectButton>
+);
+
+
+const Row = ({item, index}) => (
+  <AppleStyleSwipeableRow>
+    <Item text={item} />
+  </AppleStyleSwipeableRow>
+)
+
+
+import { Animated, I18nManager } from 'react-native';
+
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+
+class AppleStyleSwipeableRow extends Component {
+  renderLeftActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 80],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+    return (
+      <RectButton style={styles.leftAction} onPress={this.close}>
+        <AnimatedIcon
+          name="archive"
+          size={30}
+          color="#fff"
+          style={[styles.actionIcon, { transform: [{ scale }] }]}
+        />
+      </RectButton>
+    );
+  };
+  renderRightActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [-80, 0],
+      outputRange: [1, 0],
+      extrapolate: 'clamp',
+    });
+    return (
+      <RectButton style={styles.rightAction} onPress={this.close}>
+        <AnimatedIcon
+          name="delete-forever"
+          size={30}
+          color="#fff"
+          style={[styles.actionIcon, { transform: [{ scale }] }]}
+        />
+      </RectButton>
+    );
+  };
+  updateRef = ref => {
+    this._swipeableRow = ref;
+  };
+  close = () => {
+    this._swipeableRow.close();
+  };
+  render() {
+    const { children } = this.props;
+    return (
+      <Swipeable
+        ref={this.updateRef}
+        friction={2}
+        leftThreshold={80}
+        rightThreshold={40}
+        renderLeftActions={this.renderLeftActions}
+        renderRightActions={this.renderRightActions}>
+        {children}
+      </Swipeable>
+    );
+  }
 }
--- src/themes/light/App.js
+++ src/themes/light/App.js
@@ -1,4 +1,6 @@
 import {StyleSheet} from 'react-native';
+import { I18nManager } from 'react-native';
+
 
 /**
  * App Stylesheet for the light theme
@@ -7,12 +9,12 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#eae7ba',
+    backgroundColor: '#ffd14d',
   },
   title: {
     paddingHorizontal: 16,
     paddingBottom: 8,
-    color: '#353232',
+    color: '#ff004d',
     fontSize: 20,
     fontWeight: 'bold',
   },
@@ -30,5 +32,40 @@ export default StyleSheet.create({
   accent: {
     color: 'rgb(187, 192, 247)',
     fontWeight: 'bold',
+  },
+  rectButton: {
+    flex: 1,
+    height: 80,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  fromText: {
+    fontWeight: 'bold',
+    backgroundColor: 'transparent',
+  },
+  messageText: {
+    color: '#999',
+    backgroundColor: 'transparent',
+  },
+  leftAction: {
+    flex: 1,
+    backgroundColor: '#388e3c',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flexDirection: I18nManager.isRTL ? 'row' : 'row-reverse'
+  },
+  actionIcon: {
+    width: 30,
+    marginHorizontal: 10
+  },
+  rightAction: {
+    alignItems: 'center',
+    flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
+    backgroundColor: '#dd2c00',
+    flex: 1,
+    justifyContent: 'flex-end'
   }
 });
